f703a901d818dc82e213caeea7fc1d7d5bd08aec
Mubina <mubina3120@gmail.com>
2023-11-08 08:30:15 +0900

    feat: standard integration of MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import '../css/App.css';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Container> 
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant='h4' component={"h1"} gutterBottom>
+            Create React App on TypeScript with REDUX
+          </Typography>
+        </Box>
+        <Button variant='contained'>Constained</Button>
+      </Stack>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; 
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import './css/index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
-root.render(
+const container = document.getElementById('root')!;
+const root = createRoot(container); 
+
+root.render( 
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  // document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
